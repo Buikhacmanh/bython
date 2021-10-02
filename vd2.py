c372689a4cac5aac619e30f7da90f9317fb36058
Buikhacmanh <90489141+Buikhacmanh@users.noreply.github.com>
2021-10-02 10:19:11 +0700

    Add files via upload

---
+++ vd2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+series_ex = {"a":[1, 4, 6, 7, 8], "b":[9, 10, 11, 12, 13], "c":[14, 15, 16, 17, 18]}
+
+series_data = pd.DataFrame(series_ex)
+
+print(series_data)
+print("Xuat ra cot dau cua series")
+print(series_data.iloc[:, 0])
